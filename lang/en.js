4e289322b88c50cf4466a180f2d68cf9bc699f8a
glabk0023-star <glabk0023@gmail.com>
2026-07-30 23:21:12 +0430

    Create lang/en.js

---
+++ lang/en.js
@@ -0,0 +1,40 @@
+// ===============================
+// Tasfia System V1.0
+// English Language
+// ===============================
+
+export default {
+
+APP_NAME: "Tasfia System",
+
+LOGIN: "Login",
+
+LOGOUT: "Logout",
+
+HOME: "Home",
+
+DASHBOARD: "Dashboard",
+
+REGISTER: "New Registration",
+
+SEARCH: "Search",
+
+REPORTS: "Reports",
+
+ADMIN: "Admin",
+
+SETTINGS: "Settings",
+
+SAVE: "Save",
+
+RESET: "Reset",
+
+SEARCH_BUTTON: "Search",
+
+FORM_NUMBER: "Form Number",
+
+TAZKIRA_NUMBER: "Tazkira Number",
+
+PHONE_NUMBER: "Phone Number"
+
+};
