4d2e4773e8279769e3a4c5ebd25987be36c0ea93
glabk0023-star <glabk0023@gmail.com>
2026-07-30 23:20:17 +0430

    Create lang/ur.js

---
+++ lang/ur.js
@@ -0,0 +1,40 @@
+// ===============================
+// Tasfia System V1.0
+// Urdu Language
+// ===============================
+
+export default {
+
+APP_NAME: "تصفیہ سسٹم",
+
+LOGIN: "لاگ اِن",
+
+LOGOUT: "لاگ آؤٹ",
+
+HOME: "ہوم",
+
+DASHBOARD: "ڈیش بورڈ",
+
+REGISTER: "نیا اندراج",
+
+SEARCH: "تلاش",
+
+REPORTS: "رپورٹس",
+
+ADMIN: "ایڈمن",
+
+SETTINGS: "ترتیبات",
+
+SAVE: "محفوظ کریں",
+
+RESET: "صاف کریں",
+
+SEARCH_BUTTON: "تلاش",
+
+FORM_NUMBER: "فارم نمبر",
+
+TAZKIRA_NUMBER: "تذکرہ نمبر",
+
+PHONE_NUMBER: "فون نمبر"
+
+};
